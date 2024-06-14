25847e14ed03c4c1554d02bfc06edb48283cc6b3
Yann Pravo <yann.pravo@gmail.com>
2024-06-14 15:16:43 +0200

    add rubik font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 module.exports = {
   darkMode: ['class'],
   content: [
@@ -30,6 +32,9 @@ module.exports = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      fontFamily: {
+        sans: ['Rubik', ...defaultTheme.fontFamily.sans],
       },
     },
   },
